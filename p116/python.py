6aa17e8e984233896fd54f26bf54740b6deabcf1
amangorach <91610050+amangorach@users.noreply.github.com>
2022-11-27 15:09:59 +0530

    Add files via upload

---
+++ p116/python.py
@@ -0,0 +1,69 @@
+import cv2
+img = cv2.imread("solar-system.jpg")
+
+cv2.putText(img,
+        "Sun",
+        (20,300),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Mercury",
+        (100,250),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Venus",
+        (200,175),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Earth",
+        (290,275),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "mars",
+        (390,175),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Jupiter",
+        (550,375),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Saturn",
+        (750,125),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Uranus",
+        (975,300),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Neptune",
+        (1100,150),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.imshow("output",img)
+cv2.waitKey(0)
+print(img)
